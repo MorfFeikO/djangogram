3778ba063fd5ed74600421d0c14e54ea8eccc2f4
MorfFeikO <a.s.bozbei@gmail.com>
2021-03-25 21:47:05 +0200

    changed djangogram.settings in wsgi

--- djangogram/djangogram/wsgi.py
+++ djangogram/djangogram/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangogram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangogram.djangogram.settings')
 
 application = get_wsgi_application()
